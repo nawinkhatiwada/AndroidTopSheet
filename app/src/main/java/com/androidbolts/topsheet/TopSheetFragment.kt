4163cd32852872e4c0bc17d29e5478113d5cbb28
Nabin Khatiwada <nawin@Nabins-MacBook-Pro.local>
2020-05-01 12:58:58 +0545

    added toast

--- app/src/main/java/com/androidbolts/topsheet/TopSheetFragment.kt
+++ app/src/main/java/com/androidbolts/topsheet/TopSheetFragment.kt
@@ -1,9 +1,11 @@
 package com.androidbolts.topsheet
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.androidbolts.topsheet.databinding.FragmentTopSheetBinding
@@ -16,6 +18,11 @@ class TopSheetFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_top_sheet, null, false)
+        binding.btnClick.setOnClickListener {
+            val toast = Toast.makeText(requireContext(), "Click Click", Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.CENTER, 0, 0)
+            toast.show()
+        }
         return binding.root
     }
 }
